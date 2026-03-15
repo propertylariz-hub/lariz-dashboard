6a37abb5bddf5c1ded6b34f090e0582fd9432c8b
propertylariz-hub <propertylariz@gmail.com>
2026-03-15 11:23:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,50 +44,36 @@ async function gasPost(body) {
 
 // ─── MD5 ─────────────────────────────────────────────────────────────────────
 function md5(str) {
-  function safeAdd(x,y){const l=(x&0xffff)+(y&0xffff);return(((x>>16)+(y>>16)+(l>>16))<<16)|(l&0xffff);}
-  function bitRotateLeft(num,cnt){return(num<<cnt)|(num>>>(32-cnt));}
-  function md5cmn(q,a,b,x,s,t){return safeAdd(bitRotateLeft(safeAdd(safeAdd(a,q),safeAdd(x,t)),s),b);}
-  function md5ff(a,b,c,d,x,s,t){return md5cmn((b&c)|((~b)&d),a,b,x,s,t);}
-  function md5gg(a,b,c,d,x,s,t){return md5cmn((b&d)|(c&(~d)),a,b,x,s,t);}
-  function md5hh(a,b,c,d,x,s,t){return md5cmn(b^c^d,a,b,x,s,t);}
-  function md5ii(a,b,c,d,x,s,t){return md5cmn(c^(b|(~d)),a,b,x,s,t);}
-  function str2binl(str){
-    const bin={};
-    for(let i=0;i<str.length*8;i+=8) bin[i>>5]|=(str.charCodeAt(i/8)&0xff)<<(i%32);
-    return bin;
+  function L(k,d){return(k<<d)|(k>>>(32-d));}
+  function K(G,k){var I,d,F,H,x;F=(G&2147483648);H=(k&2147483648);I=(G&1073741824);d=(k&1073741824);x=(G&1073741823)+(k&1073741823);if(I&d){return(x^2147483648^F^H);}if(I|d){if(x&1073741824){return(x^3221225472^F^H);}else{return(x^1073741824^F^H);}}else{return(x^F^H);}}
+  function r(d,F,k,I,G,H,x){d=K(d,K(K((F&k)|(~F&I),G),x));return K(L(d,H),F);}
+  function q(d,F,k,I,G,H,x){d=K(d,K(K((F&I)|(k&~I),G),x));return K(L(d,H),F);}
+  function p(d,F,k,I,G,H,x){d=K(d,K(K(F^k^I,G),x));return K(L(d,H),F);}
+  function n(d,F,k,I,G,H,x){d=K(d,K(K(k^(F|~I),G),x));return K(L(d,H),F);}
+  function u(G){var F=G.length;var x=F+8;var k=(x-(x%64))/64;var I=(k+1)*16;var H=new Array(I-1);var d=0;var G2=0;while(G2<F){var q2=(G2-(G2%4))/4;d=G2%4*8;H[q2]=(H[q2]|(G.charCodeAt(G2)<<d));G2++;}var q3=(G2-(G2%4))/4;d=G2%4*8;H[q3]=H[q3]|(128<<d);H[I-2]=F<<3;H[I-1]=F>>>29;return H;}
+  function t(G){var F="";var k="";var H;for(var d=0;d<=3;d++){H=(G>>>(d*8))&255;k="0"+H.toString(16);F=F+k.substr(k.length-2,2);}return F;}
+  function s(G){var d="";for(var F=0;F<G.length;F++){d=d+t(G[F]);}return d;}
+  var G=u(str);var F,H,d,I,x;var k=1732584193;var q2=-271733879;var p2=-1732584194;var n2=271733878;
+  for(F=0;F<G.length;F+=16){H=k;d=q2;I=p2;x=n2;
+    k=r(k,q2,p2,n2,G[F+0],7,-680876936);n2=r(n2,k,q2,p2,G[F+1],12,-389564586);p2=r(p2,n2,k,q2,G[F+2],17,606105819);q2=r(q2,p2,n2,k,G[F+3],22,-1044525330);
+    k=r(k,q2,p2,n2,G[F+4],7,-176418897);n2=r(n2,k,q2,p2,G[F+5],12,1200080426);p2=r(p2,n2,k,q2,G[F+6],17,-1473231341);q2=r(q2,p2,n2,k,G[F+7],22,-45705983);
+    k=r(k,q2,p2,n2,G[F+8],7,1770035416);n2=r(n2,k,q2,p2,G[F+9],12,-1958414417);p2=r(p2,n2,k,q2,G[F+10],17,-42063);q2=r(q2,p2,n2,k,G[F+11],22,-1990404162);
+    k=r(k,q2,p2,n2,G[F+12],7,1804603682);n2=r(n2,k,q2,p2,G[F+13],12,-40341101);p2=r(p2,n2,k,q2,G[F+14],17,-1502002290);q2=r(q2,p2,n2,k,G[F+15],22,1236535329);
+    k=q(k,q2,p2,n2,G[F+1],5,-165796510);n2=q(n2,k,q2,p2,G[F+6],9,-1069501632);p2=q(p2,n2,k,q2,G[F+11],14,643717713);q2=q(q2,p2,n2,k,G[F+0],20,-373897302);
+    k=q(k,q2,p2,n2,G[F+5],5,-701558691);n2=q(n2,k,q2,p2,G[F+10],9,38016083);p2=q(p2,n2,k,q2,G[F+15],14,-660478335);q2=q(q2,p2,n2,k,G[F+4],20,-405537848);
+    k=q(k,q2,p2,n2,G[F+9],5,568446438);n2=q(n2,k,q2,p2,G[F+14],9,-1019803690);p2=q(p2,n2,k,q2,G[F+3],14,-187363961);q2=q(q2,p2,n2,k,G[F+8],20,1163531501);
+    k=q(k,q2,p2,n2,G[F+13],5,-1444681467);n2=q(n2,k,q2,p2,G[F+2],9,-51403784);p2=q(p2,n2,k,q2,G[F+7],14,1735328473);q2=q(q2,p2,n2,k,G[F+12],20,-1926607734);
+    k=p(k,q2,p2,n2,G[F+5],4,-378558);n2=p(n2,k,q2,p2,G[F+8],11,-2022574463);p2=p(p2,n2,k,q2,G[F+11],16,1839030562);q2=p(q2,p2,n2,k,G[F+14],23,-35309556);
+    k=p(k,q2,p2,n2,G[F+1],4,-1530992060);n2=p(n2,k,q2,p2,G[F+4],11,1272893353);p2=p(p2,n2,k,q2,G[F+7],16,-155497632);q2=p(q2,p2,n2,k,G[F+10],23,-1094730640);
+    k=p(k,q2,p2,n2,G[F+13],4,681279174);n2=p(n2,k,q2,p2,G[F+0],11,-358537222);p2=p(p2,n2,k,q2,G[F+3],16,-722521979);q2=p(q2,p2,n2,k,G[F+6],23,76029189);
+    k=p(k,q2,p2,n2,G[F+9],4,-640364487);n2=p(n2,k,q2,p2,G[F+12],11,-421815835);p2=p(p2,n2,k,q2,G[F+15],16,530742520);q2=p(q2,p2,n2,k,G[F+2],23,-995338651);
+    k=n(k,q2,p2,n2,G[F+0],6,-198630844);n2=n(n2,k,q2,p2,G[F+7],10,1126891415);p2=n(p2,n2,k,q2,G[F+14],15,-1416354905);q2=n(q2,p2,n2,k,G[F+5],21,-57434055);
+    k=n(k,q2,p2,n2,G[F+12],6,1700485571);n2=n(n2,k,q2,p2,G[F+3],10,-1894986606);p2=n(p2,n2,k,q2,G[F+10],15,-1051523);q2=n(q2,p2,n2,k,G[F+1],21,-2054922799);
+    k=n(k,q2,p2,n2,G[F+8],6,1873313359);n2=n(n2,k,q2,p2,G[F+15],10,-30611744);p2=n(p2,n2,k,q2,G[F+6],15,-1560198380);q2=n(q2,p2,n2,k,G[F+13],21,1309151649);
+    k=n(k,q2,p2,n2,G[F+4],6,-145523070);n2=n(n2,k,q2,p2,G[F+11],10,-1120210379);p2=n(p2,n2,k,q2,G[F+2],15,718787259);q2=n(q2,p2,n2,k,G[F+9],21,-343485551);
+    k=K(k,H);q2=K(q2,d);p2=K(p2,I);n2=K(n2,x);
   }
-  function binl2hex(binarray){
-    const hex="0123456789abcdef";let str="";
-    for(let i=0;i<binarray.length*4;i++) str+=hex.charAt((binarray[i>>2]>>((i%4)*8+4))&0xf)+hex.charAt((binarray[i>>2]>>((i%4)*8))&0xf);
-    return str;
-  }
-  function coreMD5(x,len){
-    x[len>>5]|=0x80<<((len)%32);x[(((len+64)>>>9)<<4)+14]=len;
-    let a=1732584193,b=-271733879,c=-1732584194,d=271733878;
-    for(let i=0;i<x.length;i+=16){
-      const [oa,ob,oc,od]=[a,b,c,d];
-      a=md5ff(a,b,c,d,x[i+ 0], 7,-680876936);d=md5ff(d,a,b,c,x[i+ 1],12,-389564586);c=md5ff(c,d,a,b,x[i+ 2],17, 606105819);b=md5ff(b,c,d,a,x[i+ 3],22,-1044525330);
-      a=md5ff(a,b,c,d,x[i+ 4], 7,-176418897);d=md5ff(d,a,b,c,x[i+ 5],12, 1200080426);c=md5ff(c,d,a,b,x[i+ 6],17,-1473231341);b=md5ff(b,c,d,a,x[i+ 7],22,-45705983);
-      a=md5ff(a,b,c,d,x[i+ 8], 7, 1770035416);d=md5ff(d,a,b,c,x[i+ 9],12,-1958414417);c=md5ff(c,d,a,b,x[i+10],17,-42063);b=md5ff(b,c,d,a,x[i+11],22,-1990404162);
-      a=md5ff(a,b,c,d,x[i+12], 7, 1804603682);d=md5ff(d,a,b,c,x[i+13],12,-40341101);c=md5ff(c,d,a,b,x[i+14],17,-1502002290);b=md5ff(b,c,d,a,x[i+15],22, 1236535329);
-      a=md5gg(a,b,c,d,x[i+ 1], 5,-165796510);d=md5gg(d,a,b,c,x[i+ 6], 9,-1069501632);c=md5gg(c,d,a,b,x[i+11],14, 643717713);b=md5gg(b,c,d,a,x[i+ 0],20,-373897302);
-      a=md5gg(a,b,c,d,x[i+ 5], 5,-701558691);d=md5gg(d,a,b,c,x[i+10], 9, 38016083);c=md5gg(c,d,a,b,x[i+15],14,-660478335);b=md5gg(b,c,d,a,x[i+ 4],20,-405537848);
-      a=md5gg(a,b,c,d,x[i+ 9], 5, 568446438);d=md5gg(d,a,b,c,x[i+14], 9,-1019803690);c=md5gg(c,d,a,b,x[i+ 3],14,-187363961);b=md5gg(b,c,d,a,x[i+ 8],20, 1163531501);
-      a=md5gg(a,b,c,d,x[i+13], 5,-1444681467);d=md5gg(d,a,b,c,x[i+ 2], 9,-51403784);c=md5gg(c,d,a,b,x[i+ 7],14, 1735328473);b=md5gg(b,c,d,a,x[i+12],20,-1926607734);
-      a=md5hh(a,b,c,d,x[i+ 5], 4,-378558);d=md5hh(d,a,b,c,x[i+ 8],11,-2022574463);c=md5hh(c,d,a,b,x[i+11],16, 1839030562);b=md5hh(b,c,d,a,x[i+14],23,-35309556);
-      a=md5hh(a,b,c,d,x[i+ 1], 4,-1530992060);d=md5hh(d,a,b,c,x[i+ 4],11, 1272893353);c=md5hh(c,d,a,b,x[i+ 7],16,-155497632);b=md5hh(b,c,d,a,x[i+10],23,-1094730640);
-      a=md5hh(a,b,c,d,x[i+13], 4, 681279174);d=md5hh(d,a,b,c,x[i+ 0],11,-358537222);c=md5hh(c,d,a,b,x[i+ 3],16,-722521979);b=md5hh(b,c,d,a,x[i+ 6],23, 76029189);
-      a=md5hh(a,b,c,d,x[i+ 9], 4,-640364487);d=md5hh(d,a,b,c,x[i+12],11,-421815835);c=md5hh(c,d,a,b,x[i+15],16, 530742520);b=md5hh(b,c,d,a,x[i+ 2],23,-995338651);
-      a=md5ii(a,b,c,d,x[i+ 0], 6,-198630844);d=md5ii(d,a,b,c,x[i+ 7],10, 1126891415);c=md5ii(c,d,a,b,x[i+14],15,-1416354905);b=md5ii(b,c,d,a,x[i+ 5],21,-57434055);
-      a=md5ii(a,b,c,d,x[i+12], 6, 1700485571);d=md5ii(d,a,b,c,x[i+ 3],10,-1894986606);c=md5ii(c,d,a,b,x[i+10],15,-1051523);b=md5ii(b,c,d,a,x[i+ 1],21,-2054922799);
-      a=md5ii(a,b,c,d,x[i+ 8], 6, 1873313359);d=md5ii(d,a,b,c,x[i+15],10,-30611744);c=md5ii(c,d,a,b,x[i+ 6],15,-1560198380);b=md5ii(b,c,d,a,x[i+13],21, 1309151649);
-      a=md5ii(a,b,c,d,x[i+ 4], 6,-145523070);d=md5ii(d,a,b,c,x[i+11],10,-1120210379);c=md5ii(c,d,a,b,x[i+ 2],15, 718787259);b=md5ii(b,c,d,a,x[i+ 9],21,-343485551);
-      a=safeAdd(a,oa);b=safeAdd(b,ob);c=safeAdd(c,oc);d=safeAdd(d,od);
-    }
-    return[a,b,c,d];
-  }
-  const binl=str2binl(str);
-  return binl2hex(coreMD5(binl,str.length*8));
+  return s([k,q2,p2,n2]);
 }
 
 // ─── PASSWORD STORE ───────────────────────────────────────────────────────────
